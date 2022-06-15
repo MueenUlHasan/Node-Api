0c5758a157bc240e9efac98b27bcb3823cb9605e
MueenUlHasan <106316575+MueenUlHasan@users.noreply.github.com>
2022-06-15 18:16:45 +0500

    Mueen

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
-import express from "express";
-import mongoose from "mongoose"
-import bodyParser from "body-parser"
-import router from './routes/route'
-async function App() {
-    const app=express();
-mongoose.connect("mongodb://localhost:27017/authentication")
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(router)
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import router from './routes/route';
 
-app.listen(8000)
+async function App() {
+  const app = express();
+  mongoose.connect('mongodb://localhost:27017/authentication');
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(router);
+  app.listen(8000);
 }
 export default App;
--- src/routes/route.ts
+++ src/routes/route.ts
@@ -1,7 +1,7 @@
-import express from "express";
-import { addUser, logIn } from "../controllers/user.contollers";
-import { createLoginSchema, createUserSchema } from "../interface/user.interface";
-import validateResource from "../middleware/validate.user";
+import express from 'express';
+import { addUser, logIn } from '../controllers/user.contollers';
+import { createLoginSchema, createUserSchema } from '../interface/user.interface';
+import validateResource from '../middleware/validate.user';
 
 const router = express.Router();
 
