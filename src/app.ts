f4aad189b57819d538a444fcaa75c641b6b9e5cb
MueenUlHasan <106316575+MueenUlHasan@users.noreply.github.com>
2022-06-15 16:30:42 +0500

    Mueen

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
-import express from "express"
+import express from "express";
 import mongoose from "mongoose"
 import bodyParser from "body-parser"
 import router from './routes/route'
-const app=express()
+async function App() {
+    const app=express();
 mongoose.connect("mongodb://localhost:27017/authentication")
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(router)
 
 app.listen(8000)
+}
+export default App;
